b239695ce1bb37832a317c235c298e2a74b7684e
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-07 21:46:57 +0100

    Task10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,7 +11,7 @@ int check_cycle(listint_t *list)
 	listint_t *slow = list;
 	listint_t *fast = list;
 
-	while (slow != NULL && fast != NULL)
+	while (slow -> next != NULL && fast -> next -> next != NULL)
 	{
 		slow = slow -> next;
 		fast = fast -> next -> next;
