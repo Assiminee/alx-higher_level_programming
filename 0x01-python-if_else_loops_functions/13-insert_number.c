cd6e5d77e49b98e66a6f230b974fea689b473f44
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-08 16:03:40 +0100

    Handled Empty List

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -6,10 +6,10 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *current = *head;
 	listint_t *temp;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (NULL);
 
-	if ((*head) -> next == NULL)
+	if (*head == NULL || (*head) -> next == NULL)
 		return (add_nodeint_end(head, number));
 	
 	new = malloc(sizeof(listint_t));
