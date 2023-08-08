b0e4653b30f3eb0f6f0c19bd4409ef54c0ae8bf1
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-08 14:34:36 +0100

    Task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,14 +3,14 @@ for i in range(100):
     sep = ""
 
     prefix = ""
-    
+
     if i < 10:
         prefix = "0"
-    
+
     if i == 99:
         sep = "\n"
-    
+
     if i != 0:
         print(",", end=" ")
-    
+
     print("{:s}{:d}".format(prefix, i), end=sep)
