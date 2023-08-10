b1997e067fd34ba0d3274f0ef0714468abba8684
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-10 17:30:15 +0100

    TASK6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    ops = ["+", "-", "*", "/"]
+    op = argv[2]
+    if op not in ops:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+
+    function_list = [add, sub, mul, div]
+    for i in range(len(ops)):
+        if op == ops[i]:
+            print("{:d} {:s} {:d} = {:d}".format(a, op, b, function_list[i](a, b)))
