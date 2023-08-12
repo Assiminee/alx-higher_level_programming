a204ac0760b8103ed76a8dfbeb14fc591d50e2f4
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-12 21:08:54 +0100

    100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -4,12 +4,19 @@
 
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t obj_size = PyList_Size(p), allocated = 0, i;
-		printf("[*] Size of the Python List = %ld\n", obj_size);
-		for (i = 0; i < obj_size; i++)
-			allocated += sizeof(p);
-		printf("[*] Allocated = %ld\n", allocated);
-		for (i = 0; i < obj_size; i++)
-			printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i)) -> tp_name);
-	}
+	Py_ssize_t obj_size = PyList_Size(p), i;
+	char **types = malloc(obj_size * sizeof(char*));
+
+	printf("[*] Size of the Python List = %ld\n", obj_size);
+	for (i = 0; i < obj_size; i++)
+	{
+		types[i] = malloc(strlen(Py_TYPE(PyList_GetItem(p, i)) -> tp_name) + 1);
+		strcpy(types[i], Py_TYPE(PyList_GetItem(p, i)) -> tp_name);
+	}	
+	printf("[*] Allocated = %ld\n", PyList_ClearFreeList());
+	for (i = 0; i < obj_size; i++)
+		printf("Element %ld: %s\n", i, types[i]);
+	for (i = 0; i < obj_size; i++)
+		free(types[i]);
+	free(types)
 }
