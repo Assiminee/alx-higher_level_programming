a02f49aabc2429dbb89e993d2cb191e2b5e409cd
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-12 17:27:31 +0100

    4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
     newList = []
     newList.extend(my_list)
-    if 0 < idx < len(my_list):
+    if idx >= 0 and idx < len(my_list):
         newList[idx] = element
     return newList
