3add773b5f2f4d963f699965ed81af4d6259405f
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-12 17:34:08 +0100

    6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        sep = " "
-        for i in range(len(row)):
-            if i == len(row) - 1:
-                sep = "\n"
-            print("{:d}".format(row[i]), end=sep)
+    if matrix:
+        for row in matrix:
+            if row:
+                sep = " "
+                for i in range(len(row)):
+                    if i == len(row) - 1:
+                        sep = "\n"
+                    print("{:d}".format(row[i]), end=sep)
