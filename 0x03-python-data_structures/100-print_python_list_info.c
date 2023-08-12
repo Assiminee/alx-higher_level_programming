59fba94c797c991921f5b88d487a6f0884a2b501
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-12 20:20:28 +0000

    100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,7 +7,7 @@ void print_python_list_info(PyObject *p)
 	Py_ssize_t obj_size = PyList_Size(p), i;
 
 	printf("[*] Size of the Python List = %ld\n", obj_size);
-	printf("[*] Allocated = %ld\n", p -> allocated);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p) -> allocated);
 	for (i = 0; i < obj_size; i++)
 		printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i)) -> tp_name);
 }
