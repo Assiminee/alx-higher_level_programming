229f1608d21c2f09fd9f7445cbfa522e1e74469b
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-16 21:49:06 +0100

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if isinstance(roman_string, str):
-        numerals = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
+        numerals = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
         first_char = roman_string[0]
         if (first_char == 'I' and len(roman_string) > 1):
             num = -numerals['I']
@@ -11,4 +11,3 @@ def roman_to_int(roman_string):
             num += numerals[roman_string[i]]
         return num
     return 0
-
