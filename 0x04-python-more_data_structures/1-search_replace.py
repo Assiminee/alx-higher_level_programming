de053e33866bb206a7f0e1adb2eb99fc68ef9701
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-16 20:26:41 +0100

    1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if (my_list):
+        new_list = list(map(lambda x: x if x != search else replace, my_list))
+        return new_list
