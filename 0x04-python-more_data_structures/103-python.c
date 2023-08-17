ee95d5dc7b58910c39626040a14ed71f981484e7
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-17 16:47:50 +0000

    last

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,6 +1,7 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "Python.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+#include <string.h>
 
 void print_python_list(PyObject *p)
 {
@@ -10,7 +11,7 @@ void print_python_list(PyObject *p)
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
 	printf("[*] Allocated = %ld\n", ListObj->allocated);
-	while (i < 2)
+	while (i < PyList_Size(p))
 	{
 		printf("Element %ld: %s\n", i, ListObj->ob_item[i]->ob_type->tp_name);
 		i++;
@@ -19,9 +20,31 @@ void print_python_list(PyObject *p)
 
 void print_python_bytes(PyObject *p)
 {
-	PyBytesObject *BytesObj = (PyBytesObject *)p;
+	PyBytesObject *BytesObj;
+	Py_ssize_t ObjSize, i;
+
+	if (!PyBytes_CheckExact(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		exit(EXIT_FAILURE);
+	}
+	BytesObj = (PyBytesObject *)p;
+	ObjSize = PyBytes_Size(p);
+
 	printf("[.] bytes object info\n");
-	printf("  size: %ld\n", PyBytes_Size(p));
-	printf("  trying string: %s\n", PyBytes_AsString(p));
-	printf("%b\n", p[0]);
+	printf("  size: %ld\n", ObjSize);
+	printf("  trying string: %s\n", BytesObj->ob_sval);
+	if (ObjSize >= 10)
+		ObjSize = 10;
+	else
+		ObjSize++;
+	printf("  first %ld bytes: ", ObjSize);
+	for (i = 0; i < ObjSize; i++)
+	{
+		printf("%02x", (unsigned char)BytesObj->ob_sval[i]);
+		if (i == ObjSize - 1)
+			printf("\n");
+		else
+			printf(" ");
+	}
 }
--- 0x04-python-more_data_structures/103-tests.py
+++ 0x04-python-more_data_structures/103-tests.py
@@ -5,6 +5,41 @@ lib = ctypes.CDLL('./libPython.so')
 lib.print_python_list.argtypes = [ctypes.py_object]
 lib.print_python_bytes.argtypes = [ctypes.py_object]
 s = b"Hello"
+
+print("lib.print_python_bytes(s);");
 lib.print_python_bytes(s);
+b = b'\xff\xf8\x00\x00\x00\x00\x00\x00';
+print("\n\nlib.print_python_bytes(b);");
+lib.print_python_bytes(b);
+b = b'What does the \'b\' character do in front of a string literal?';
+print("\n\nlib.print_python_bytes(b);");
+lib.print_python_bytes(b);
 l = [b'Hello', b'World']
+print("\n\nlib.print_python_list(l)");
 lib.print_python_list(l)
+del l[1]
+print("\n\nlib.print_python_list(l)");
+lib.print_python_list(l)
+l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], b"Holberton", "Betty"]
+print("\n\nlib.print_python_list(l)");
+lib.print_python_list(l)
+l = []
+print("\n\nlib.print_python_list([])");
+lib.print_python_list(l)
+l.append(0)
+print("\n\nlib.print_python_list([0])");
+lib.print_python_list(l)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+print("\n\nlib.print_python_list([0 1 2 3 4])");
+lib.print_python_list(l)
+l.pop()
+print("\n\nlib.print_python_list([0 1 2 3])");
+lib.print_python_list(l)
+l = ["Holberton"]
+print("\n\nlib.print_python_list([\"Holberton\"])");
+lib.print_python_list(l)
+print("\n\nlib.print_python_bytes(l);");
+lib.print_python_bytes(l);
