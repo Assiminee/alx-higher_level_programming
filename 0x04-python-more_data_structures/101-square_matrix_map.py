79435d8f4c633639c6f0a71ead65549acaca480f
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-17 02:21:51 +0100

    14

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda l: list(map(lambda x: x**2, l)), matrix))
+    if matrix:
+        return list(map(lambda l: list(map(lambda x: x**2, l)), matrix))
