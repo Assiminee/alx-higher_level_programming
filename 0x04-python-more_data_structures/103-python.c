61fc5543fbd6a7ae952f1b84d98dd3e3eccee56f
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-17 16:59:26 +0000

    last

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -3,6 +3,9 @@
 #include <Python.h>
 #include <string.h>
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
 void print_python_list(PyObject *p)
 {
 	PyListObject *ListObj = (PyListObject *)p;
