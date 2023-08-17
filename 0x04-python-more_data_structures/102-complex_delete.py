cde6fdaea7dbda74f2786ff214944a4e7775bd4e
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-17 02:38:20 +0100

    15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -8,4 +8,4 @@ def complex_delete(a_dictionary, value):
                 keys.append(key)
         for key in keys:
             del a_dictionary[key]
-        return a_dictionary
+    return a_dictionary
