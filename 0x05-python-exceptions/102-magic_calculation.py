e43f22c89c2d817a54fcc075ec033a10c7f01029
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-28 20:47:32 +0100

    test

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from dis import dis
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -8,6 +7,5 @@ def magic_calculation(a, b):
             break
         else:
             result += (a ** b) / i
-        result = a + b
+    result = a + b
     return result
-dis(magic_calculation)
