3d2dfd1f2e34c337d5bce781a5edaa7e3cb7d771
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-30 15:30:14 +0100

    103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,7 +7,7 @@ class MagicClass:
     """ body of MagicClass """
     def __init__(self, radius=0):
         """ initializes an instance of MagicClass class """
-        if type(radius) is not int or type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             TypeError('radius must be a number')
             self.__radius = None
         else:
