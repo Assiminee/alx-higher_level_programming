abd057891505ab04f1c46aa989047ee1154ac41c
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-30 02:26:31 +0100

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" defines a square """
+
+
+class Square:
+    """ Square class body """
+    def __init__(self, size=0):
+        """ initializes an instance of Square class """
+        self.size = size
+
+    @property
+    def size(self):
+        """ a getter for the instance attribute 'size' """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ a setter for the instance attribute 'size' """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ computes and returns area of a square """
+        return self.__size ** 2
