d017d1ccb92242e21505617be96719c2387e56c2
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-30 02:10:36 +0100

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" defines a square """
+
+
+class Square:
+    """ Square class body """
+    def __init__(self, size=0):
+        """ initializes an instance of Square class """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
