99ec1d08ebf32e4177913c5e907926c188e57979
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-30 02:01:13 +0100

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" defines a square """
+
+
+class Square:
+    """ Square class body """
+    def __init__(self, size):
+        """ initializes an instance of Square class """
+        self.__size = size
