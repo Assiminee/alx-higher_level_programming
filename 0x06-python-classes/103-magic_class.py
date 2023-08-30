99c660f79869aafe401dac3c03620ff2c1e097d0
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-08-30 15:27:33 +0100

    103

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import math
+""" defines MagicClass """
+
+
+class MagicClass:
+    """ body of MagicClass """
+    def __init__(self, radius=0):
+        if type(radius) is not int or type(radius) is not float:
+            TypeError('radius must be a number')
+            self.__radius = None
+        else:
+            self.__radius = 0
+
+    def area(self):
+        """ return area of instance """
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        """ returns circumference of instance """
+        return 2 * math.pi * self.__radius
