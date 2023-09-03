f04dac38b8747efcdde6f41a929f87f0b776b82c
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-09-04 00:47:06 +0100

    doctest

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+5-text_indentation module
+"""
+
+
+def text_indentation(text):
+    """
+    prints "text" with 2 new lines after each of these
+    characters: "." "?" and ":"
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    i = 0
+    while i < len(text):
+        if text[i] in [".", "?", ":"]:
+            print("{}\n".format(text[i]))
+            if len(text) > i + 1:
+                if text[i + 1] == " ":
+                    i += 2
+                    continue
+        else:
+            print("{}".format(text[i]), end="")
+        i += 1
