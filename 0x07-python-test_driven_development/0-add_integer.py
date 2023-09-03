8d0ad826b491c03c1269222ad26383a4544a9e6c
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-09-03 20:59:03 +0100

    Tasks 0 and 1

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+adds two integers a and b
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Returns a + b
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+matrix division module
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Returns a new matrix that's the result of all
+    the elements of "matrix" divided by "div"
+    """
+    if not isinstance(matrix, list) or not len(matrix) or\
+    not all(isinstance(row, list) for row in matrix) or\
+    not all(len(row) != 0 for row in matrix) or\
+    not all(isinstance(ele, (int, float)) for row in matrix for ele in row):
+        raise TypeError("matrix must be a matrix "
+                        "(list of lists) of integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return [[round(ele / div, 2) for ele in row] for row in matrix]
