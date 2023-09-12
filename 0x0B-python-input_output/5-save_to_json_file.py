bcad014e513f62953d7010fc487ea5ce7a45c641
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-09-13 00:57:09 +0100

    fixed

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,5 +9,4 @@ def save_to_json_file(my_obj, filename):
     using a JSON representation
     """
     with open(filename, "w", encoding="utf-8") as f:
-        data = json.dumps(my_obj)
-        json.dump(data, f)
+        json.dump(my_obj, f)
