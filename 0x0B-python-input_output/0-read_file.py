7b2cfc92dd4f8034113bdd7b6624431347640ce0
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-09-13 00:24:11 +0100

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""read_file module
+"""
+
+
 def read_file(filename=""):
+    """ reads the contents of a file
+    """
     with open(filename, encoding="utf-8") as f:
         print(f"{f.read()}", end="")
