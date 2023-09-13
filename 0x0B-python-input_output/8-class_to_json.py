9c978718bef0069f82ca856193c51cb98a252ed1
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-09-13 01:53:17 +0100

    test

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -9,4 +9,4 @@ def class_to_json(obj):
     data structure (list, dictionary, string, integer
     and boolean) for JSON serialization of an object
     """
-    return obj.__dict__
+    return json.dumps(obj.__dict__)
