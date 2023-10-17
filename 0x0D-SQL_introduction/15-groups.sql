2226246d3bcaf53db78da0630511435b93de636f
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-10-17 19:26:48 +0100

    15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Counts number of lines with same score
-SELECT score, COUNT(score) AS "number" FROM second_table GROUP BY score ORDER BY "number" DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
