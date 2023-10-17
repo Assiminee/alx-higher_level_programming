2ee6f14d1424b9de9cda3764151d025b0d5942a5
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-10-17 20:42:29 +0100

    100

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,13 @@
+-- Changes character sets for database, tables, and field
+ALTER DATABASE hbtn_0c_0 
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE hbtn_0c_0.first_table
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE hbtn_0c_0.first_table
+MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
