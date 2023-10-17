8acbba288eab7128d2342df66e2e7b20bc044536
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-10-17 19:13:09 +0100

    12

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
--- Prints score and name from second_table
+-- Prints score and name from second_table where score is higher than 10
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- Updates data
+UPDATE second_table
+SET score = 10
+WHERE name = "Bob";
