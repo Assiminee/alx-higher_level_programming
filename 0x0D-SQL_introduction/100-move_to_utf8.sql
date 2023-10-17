f5a3177d57a3a697fc755e69def5207e4c3787a5
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-10-17 20:47:01 +0100

    100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,15 +1,12 @@
 -- Changes character sets for database, tables, and field
 ALTER DATABASE hbtn_0c_0 
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 USE hbtn_0c_0;
 
 ALTER TABLE first_table
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
 MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
