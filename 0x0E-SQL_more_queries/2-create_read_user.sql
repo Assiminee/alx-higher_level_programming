3cd193481a19c6451f65b80e86a6f59627145200
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-10-18 20:13:26 +0100

    2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- Creates database hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Creates user user_0d_2 with password
-CREATE USER IS NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Grants only select privilege to new user on newly created database
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
