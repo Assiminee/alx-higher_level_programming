eca2fb4e6666a0290934f97e82795d7dcf899b95
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-10-18 23:30:17 +0100

    16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
 SELECT s.title, g.name FROM tv_shows s
-INNER JOIN tv_show_genres m
+LEFT OUTER JOIN tv_show_genres m
 ON s.id = m.show_id
-INNER JOIN tv_genres g
+LEFT OUTER JOIN tv_genres g
 ON m.genre_id = g.id
 ORDER BY s.title, g.name ASC;
