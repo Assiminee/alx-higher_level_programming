ebc45262ebac1ee4573989a1f9455a6971179488
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-10-18 23:28:48 +0100

    16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+SELECT s.title, g.name FROM tv_shows s
+INNER JOIN tv_show_genres m
+ON s.id = m.show_id
+INNER JOIN tv_genres g
+ON m.genre_id = g.id
+ORDER BY s.title, g.name ASC;
