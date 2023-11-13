6b0d213caf400321ad71eb3db77f152dbadba271
Yasmine Znatni <znatni.yasmine@gmail.com>
2023-11-14 00:47:59 +0100

    8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+const parsed = parseInt(arg);
+if (!isNaN(parsed)) {
+  if (parsed > 0) {
+    for (let i = 0; i < parsed; i++) {
+      console.log('x'.repeat(parsed));
+    }
+  }
+} else {
+  console.log('Missing size');
+}
