9d7225e1c4172f6851cdd0ea211e2d16f7bf3e2e
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-02-12 23:43:16 +0100

    7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,11 +3,6 @@ const arg = process.argv[2];
 const parsed = parseInt(arg);
 if (!isNaN(parsed)) {
   if (parsed > 0) {
-    for (let i = 0; i < parsed; #!/usr/bin/node
-const arg = process.argv[2];
-const parsed = parseInt(arg);
-if (!isNaN(parsed)) {
-  if (parsed > 0) {
     for (let i = 0; i < parsed; i++) {
       console.log('C is fun');
     }
