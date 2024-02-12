d80fae0a73570b913b7c4933d0c965fb528a4bd7
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-02-12 23:41:25 +0100

    js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 const args = process.argv;
-console.log(`${args[2]} is ${args[3]}`);
-#!/usr/bin/node
-const args = process.argv;
 const parsed = parseInt(args[2]);
 if (isNaN(parsed)) {
   console.log('Not a number');
