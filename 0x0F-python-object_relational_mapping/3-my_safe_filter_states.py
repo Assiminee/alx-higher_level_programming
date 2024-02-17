b6d97cbebb68ea1a3af945780106cc44db2b9a90
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-02-17 21:06:13 +0100

    safeinput

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Fetching data from database where name matches user input
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    host = "localhost"
+    port = 3306
+    user, pwd, db = sys.argv[1:4]
+    searchTerm = sys.argv[4]
+
+    con = MySQLdb.connect(
+            host=host, port=port, user=user, passwd=pwd, db=db
+    )
+
+    safeInput = (searchTerm,)
+    query = "SELECT * FROM states\
+            WHERE name LIKE BINARY %s\
+            ORDER BY id ASC;"
+
+    cursor = con.cursor()
+    cursor.execute(query, safeInput)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    con.close()
