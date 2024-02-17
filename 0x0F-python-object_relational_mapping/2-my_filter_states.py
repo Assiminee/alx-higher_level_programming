406dc6278e2fe6fbe3091d90b6e37e759648ccce
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-02-17 20:33:56 +0100

    3

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Fetching data from database where name matches user input
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    host = "localhost"
+    port = 3306
+    user, pwd, db = sys.argv[1:4]
+    searchTerm = sys.argv[4]
+
+    con = MySQLdb.connect(
+            host=host, port=port, user=user, passwd=pwd, db=db
+    )
+
+    query = f"SELECT * FROM states\
+            WHERE name LIKE BINARY '{searchTerm}'\
+            ORDER BY id ASC;"
+    cursor = con.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    con.close()
