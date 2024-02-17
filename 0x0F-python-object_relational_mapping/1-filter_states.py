4b79e5cf7755ebadf76e467726949b80d9953dce
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-02-17 20:19:21 +0100

    task 2

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Fetching data from database where name starts with N
+sorted in ascending order of id
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    host = "localhost"
+    port = 3306
+    user, pwd, db = sys.argv[1:4]
+
+    con = MySQLdb.connect(
+            host=host, port=port, user=user, passwd=pwd, db=db
+    )
+
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
+    cursor = con.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    con.close()
