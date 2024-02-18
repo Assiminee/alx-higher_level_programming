f8b9524c3e459be582f5309f9ba3888768c2a15a
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-02-18 15:50:39 +0100

    10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -2,25 +2,23 @@
 """Fetches data from states table
 """
 from model_state import Base, State
-from sqlalchemy import asc, func
-from sqlalchemy import create_engine
-from sqlalchemy import select
-from sqlalchemy.sql import bindparam
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, select
 import sys
 
 if __name__ == "__main__":
-    con = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
-                                                      sys.argv[2],
-                                                      sys.argv[3])
-    engine = create_engine(con, pool_pre_ping=True)
+    db_connection = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+            )
+
+    engine = create_engine(db_connection, pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
 
     user_input = sys.argv[4]
 
     with engine.connect() as conn:
-        query = select(State.id).where(State.name == user_input)
-        state_id = conn.execute(query).fetchone()[0]
-        print(state_id) if state_id else print("Not found")
+        query = select(State).where(State.name == user_input)
+        state = conn.execute(query).first()
+        print(state.id) if state else print("Not found")
 
     engine.dispose()
