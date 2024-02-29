457995160b6d835af2952d2011997f62cddf8d4c
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-03-01 00:58:00 +0100

    6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Function that searches for the peak element in
+a list of integers 
+"""
+
+
+def find_peak(int_list):
+    """ Returns peak value of list of unsorted integers
+    """
+    if not int_list:
+        return None
+    left = 0
+    right = len(int_list) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if int_list[mid] >= int_list[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+    return int_list[left]
