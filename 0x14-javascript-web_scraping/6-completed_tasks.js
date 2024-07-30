fb68ddd7ae68ab5324f34699ea1d9d1a1765b1f8
Assiminee <znatni.yasmine@gmail.com>
2024-07-30 18:13:41 +0100

    msg

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+const { error, log } = require('console');
+const req = require('request');
+
+const displayCompleted = (url) => {
+  req(url, (err, resp, body) => {
+    if (err) { error(err); return; }
+    const obj = {};
+    const json = JSON.parse(body);
+
+    for (const entry of json) {
+      const userId = `${entry.userId}`;
+
+      if (!entry.completed) {
+        continue;
+      }
+
+      obj[userId] = userId in obj ? obj[userId] + 1 : 1;
+    }
+    log(obj);
+  });
+};
+
+if (process.argv.length < 3) {
+  log('Usage: ./6-completed_tasks.js https://jsonplaceholder.typicode.com/todos');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+
+displayCompleted(url);
